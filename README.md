93c4d0fcaaf0588043b32984b3aa5e2875bb5819
Christian <p-git@c5s.de>
2020-07-28 13:51:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# YAWSP
+
+Security & Privacy
---
+++ yawsp.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: YAWSP -- Yet Another Wordpress Security Plugin
+Description: A minimal plugin for enhancing security and privacy.
+Version: 0.1
+Author: chrrel
+Author URI: http://github.com/chrrel
+Text Domain: health-check
+Domain Path: /languages
+*/
