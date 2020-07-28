12db43da8d227ceccd3af71a71e3a54843e8cb18
Christian <p-git@c5s.de>
2020-07-28 21:17:32 +0200

    added settings page

---
+++ includes/admin.php
@@ -0,0 +1,43 @@
+<?php
+// Create settings page
+function myplugin_register_options_page() {
+	add_options_page('YAWSP', 'YAWSP', 'manage_options', 'yawsp', 'yawsp_options_page');
+}
+add_action('admin_menu', 'myplugin_register_options_page');
+
+function yawsp_options_page() {
+?>
+	<div class="wrap">
+	<h2>YAWSP - Yet Another Wordpress Security Plugin</h2>
+	<p>YAWSP is a minimal plugin for enhancing security and privacy of WordPress websites.</p>
+	<form method="post" action="options.php">
+	<?php settings_fields( 'myplugin_options_group' ); ?>
+	<h3>This is my option</h3>
+	<p>Some text here.</p>
+	<table>
+	<tr valign="top">
+	<th scope="row"><label for="myplugin_option_name">Label</label></th>
+	<td><input type="text" id="myplugin_option_name" name="myplugin_option_name" value="<?php echo get_option('myplugin_option_name'); ?>" /></td>
+	</tr>
+	</table>
+	<?php submit_button(); ?>
+	</form>
+	</div>
+<?php
+}
+
+// Link to settings page from plugins screen
+add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'add_action_links' );
+function add_action_links ( $links ) {
+    $mylinks = array(
+        '<a href="' . admin_url( 'options-general.php?page=yawsp' ) . '">' . __( 'Settings' ) .' </a>',
+    );
+    return array_merge( $links, $mylinks );
+}
+
+function myplugin_register_settings() {
+	add_option( 'myplugin_option_name', 'This is my option value.');
+	register_setting( 'myplugin_options_group', 'myplugin_option_name', 'myplugin_callback' );
+ }
+ add_action( 'admin_init', 'myplugin_register_settings' );
+?>
--- yawsp.php
+++ yawsp.php
@@ -9,10 +9,12 @@ Text Domain: health-check
 Domain Path: /languages
 */
 
-
+// Define plugin constants
 define('YAWSP_PATH', plugin_dir_path(__FILE__)); 
 define('YAWSP_LOG_DIRECTORY', YAWSP_PATH . "log/"); 
 
-# Include the security module
+// Include the security and privacy modules
 require_once YAWSP_PATH . 'includes/security.php';
 require_once YAWSP_PATH . 'includes/privacy.php';
+
+require_once YAWSP_PATH . 'includes/admin.php';
