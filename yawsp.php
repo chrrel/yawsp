3584d51577dacab92fe1b99203bcd7286c55d78e
Christian <p-git@c5s.de>
2020-07-29 13:14:04 +0200

    added license block

--- yawsp.php
+++ yawsp.php
@@ -11,6 +11,18 @@
  * License URI:       https://www.gnu.org/licenses/gpl-3.0
  * Text Domain:       yawsp
  * Domain Path:       /languages
+ *
+ * YAWSP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 as 
+ * published by the Free Software Foundation.
+ * 
+ * YAWSP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with YAWSP. If not, see https://www.gnu.org/licenses/gpl-3.0.
  */
 
 /* Prevent direct access to plugin files */
