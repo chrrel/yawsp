d97e0ace2a20743d701d26ec0704a552f98416ec
Christian <p-git@c5s.de>
2021-11-30 23:06:41 +0100

    replace e-mail address on comment approval

--- includes/privacy.php
+++ includes/privacy.php
@@ -66,19 +66,21 @@ function yawsp_remove_comment_cookie_field($fields) {
 add_filter('comment_form_default_fields', 'yawsp_remove_comment_cookie_field');
 
 /**
- * Remove IPs from comments.
+ * Remove IPs and e-mail addresses from comments.
  * 
- * Replace a comment's IP address with 127.0.0.1 when the comment is approved or classified as spam.
+ * Replace a comment's IP address with 127.0.0.1 and the e-mail address with comment-<ID>@example.com
+ * when the comment is approved or classified as spam.
  */
-function yawsp_remove_ip_from_comment_on_approval($new_status, $old_status, $comment) {
+function yawsp_remove_ip_and_email_from_comment_on_approval($new_status, $old_status, $comment) {
 	if(($old_status != $new_status) && ($new_status == 'approved' || $new_status == 'spam')) {
 		$modifiedComment = array();
 		$modifiedComment['comment_ID'] = $comment->comment_ID;
 		$modifiedComment['comment_author_IP'] = '127.0.0.1';
+		$modifiedComment['comment_author_email'] = 'comment-'. $comment->comment_ID .'@example.com';
 		wp_update_comment($modifiedComment);
 	}
 }
-add_action('transition_comment_status', 'yawsp_remove_ip_from_comment_on_approval', 10, 3);
+add_action('transition_comment_status', 'yawsp_remove_ip_and_email_from_comment_on_approval', 10, 3);
 
 /**
  * Replace embedded YouTube videos with youtube-nocookie.com embedds.
